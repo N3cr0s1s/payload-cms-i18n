63d4d7a49ed5dd0d9c2d2e99a7fd07d74c9a96be
N3cr0s1s <marialex958@gmail.com>
2026-02-09 19:33:25 +0100

    fix: default locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,8 @@
 import { defineRouting } from 'next-intl/routing'
 
 export const routing = defineRouting({
-  locales: ['en', 'hu'],
+  locales: ['hu', 'en'],
   defaultLocale: 'hu',
-  localePrefix: "as-needed"
+  localePrefix: "as-needed",
+  localeDetection: false
 })
